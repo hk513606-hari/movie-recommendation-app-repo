787d68741bc384dd872996514786decf68757382
hk513606-hari <hk513606@gmail.com>
2025-10-15 13:21:23 +0530

    Update omdb_utils.py hari

--- src/omdb_utils.py
+++ src/omdb_utils.py
@@ -1,9 +1,9 @@
 # omdb_utils.py
 import requests
 
-def get_movie_details(title, api_key):
+def get_movie_details(title, 65f95272 ):
 
-    url = f"http://www.omdbapi.com/?t={title}&plot=full&apikey={api_key}"
+    url = f"http://www.omdbapi.com/?t={title}&plot=full&apikey={65f95272}"
     res = requests.get(url).json()
     if res.get("Response") == "True":
         result = res.get("Plot", "N/A"), res.get("Poster", "N/A")
